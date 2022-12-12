70119102a8d9f777d3646b5db8a5bac5f03d0311
Ярослав Гурин <gurin.ucheba@gmail.com>
2022-12-12 23:15:08 +0500

    fullscreen

--- project/src/pages/player/player.tsx
+++ project/src/pages/player/player.tsx
@@ -17,22 +17,31 @@ function Player(): JSX.Element | null {
 
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [playing, setPlaying] = useState(true);
-  const videoHandler = (control:string) => {
-    if (currentFilm && videoRef.current){
-      if (control === 'play') {
-        videoRef.current.play();
-        setPlaying(true);
-        setTimeLeft(videoTime - videoRef.current?.currentTime);
-      } else if (control === 'pause' && videoRef.current) {
-        videoRef.current.pause();
-        setPlaying(false);
-      }
-    }
-  };
-
   const [videoTime, setVideoTime] = useState(0);
   const [timeLeft, setTimeLeft] = useState(0);
   const [progress, setProgress] = useState(0);
+  const videoHandler = (control:string) => {
+    if (currentFilm && videoRef.current) {
+      switch (control) {
+        case 'play':
+          videoRef.current.play();
+          setPlaying(true);
+          setTimeLeft(videoTime - videoRef.current?.currentTime);
+          break;
+        case 'pause':
+          videoRef.current.pause();
+          setPlaying(false);
+          break;
+        case 'full-screen':
+          videoRef.current?.requestFullscreen();
+          break;
+        default:
+
+          break;
+      }
+    }
+
+  };
   React.useEffect(() => {
     setTimeout(() => {
       if (videoRef.current){
@@ -55,8 +64,6 @@ function Player(): JSX.Element | null {
         <div className="player">
           <video id={currentFilm.name} onLoadedMetadata={handleLoadedMetadata} src={currentFilm.videoLink} ref={videoRef} autoPlay className="player__video" poster="img/player-poster.jpg">
           </video>
-
-
           <Link to={`/films/${currentFilm.id}`} type="button" className="player__exit">
             Exit
           </Link>
@@ -68,7 +75,7 @@ function Player(): JSX.Element | null {
                 <div className="player__toggle" style={{left: '30%'}}>
                 </div>
               </div>
-              <div className="player__time-value">{`${Math.floor(timeLeft / 60) }:${ (`0${ Math.floor(timeLeft % 60)}`).slice(-2)}`}</div>
+              <div className="player__time-value">{(timeLeft < 3600) ? `${Math.floor(timeLeft / 60) }:${ (`0${ Math.floor(timeLeft % 60)}`).slice(-2)}` : `${Math.floor(timeLeft / 3600) }:${Math.floor((timeLeft % 3600) / 60) }:${ (`0${ Math.floor(timeLeft % 60)}`).slice(-2)}`}</div>
             </div>
 
             <div className="player__controls-row">
@@ -98,7 +105,9 @@ function Player(): JSX.Element | null {
                   </button>)}
               <div className="player__name">{currentFilm.name}</div>
 
-              <button type="button" className="player__full-screen">
+              <button onClick={() => videoHandler('full-screen')}
+                type="button" className="player__full-screen"
+              >
                 <svg viewBox="0 0 27 27" width="27" height="27">
                   <use xlinkHref="#full-screen"></use>
                 </svg>
